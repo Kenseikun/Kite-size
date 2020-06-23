52a82f190e72181f4e4866fe7436545c00035757
Sebastian Dziechciarz <sebastian.dziechciarz@wp.pl>
2020-06-23 15:47:18 +0200

    without API

--- js/app.js
+++ js/app.js
@@ -11,6 +11,15 @@ const App = () => {
 
     const dateBuilder = (d) => {
         let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+        let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+
+        let day = days[d.getDay()];
+        let date = d.getDate();
+        let month = months[d.getMonth()];
+        let year = d.getFullYear();
+
+        return `${day} - ${date} - ${month} - ${year}`
     }
 
 
@@ -24,8 +33,12 @@ const App = () => {
                     </input>
                 </div>
                 <div className="location-box">
-                    <div className="location">Warsaw,PL</div>
+                    <div className="location">Warsaw , PL</div>
                     <div className="date">{dateBuilder(new Date())}</div>
+                </div>
+                <div className="weather-box">
+                    <div className="temp">15*c</div>
+                    <div className="weather">Sunny</div>
                 </div>
             </main>
         </div>
